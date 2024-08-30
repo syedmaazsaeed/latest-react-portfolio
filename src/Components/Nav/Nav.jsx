5065b599fce694a8134224814148e5e24fcaedb5
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-08-30 21:24:08 +0500

    set nav items

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import './Nav.css';
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { BiBriefcase } from "react-icons/bi";
+import { BiMessageSquareDetail } from "react-icons/bi";
+
+
+
+
+
+
 
 const Nav = () => {
   return (
-    <div>
-      Navigation Items
-    </div>
+    <nav>
+      <a href="#"><AiOutlineHome /></a>
+      <a href="#about"><AiOutlineUser /></a>
+      <a href="#experience"><BiBook /></a>
+      <a href="#services"><RiServiceLine /></a>
+      <a href="#portfolio"><BiBriefcase /></a>
+      <a href="#contact"><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
