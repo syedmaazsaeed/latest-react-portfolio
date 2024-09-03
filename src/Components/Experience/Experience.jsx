2fa89896a0b7f6d3fef87f6a0261060c453596b5
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-03 22:39:31 +0500

    add experience section

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -5,7 +5,18 @@ import './Experience.css';
 const Experience = () => {
   return (
     <section id='experience'>
-      Experience Section
+      
+      <h5>What Skills I Have</h5>
+      <h2>My Work Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          
+        </div>
+        <div className="experience__backend">
+
+        </div>
+      </div>
     </section>
   )
 }
