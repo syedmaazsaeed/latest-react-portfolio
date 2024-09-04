49a038f1a57e6dcb61103a22de8260a5ea80c6ad
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-04 12:16:35 +0500

    add content in experience section

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -18,33 +18,45 @@ const Experience = () => {
           <div className="experience__content">
             <article className='experience__details'>
             <BsFillPatchCheckFill />
+            <div>
             <h4>HTML</h4>
             <small className='text-light'>Experienced</small>
+            </div>
             </article>
             <article className='experience__details'>
             <BsFillPatchCheckFill />
+            <div>
             <h4>CSS</h4>
             <small className='text-light'>Intermediate</small>
+            </div>
             </article>
             <article className='experience__details'>
             <BsFillPatchCheckFill />
-            <h4>JavaScript</h4>
-            <small className='text-light'>Expert</small>
+           <div>
+           <h4>JavaScript</h4>
+           <small className='text-light'>Expert</small>
+           </div>
             </article>
             <article className='experience__details'>
             <BsFillPatchCheckFill />
+            <div>
             <h4>Bootstrap</h4>
             <small className='text-light'>Experienced</small>
+            </div>
             </article>
             <article className='experience__details'>
             <BsFillPatchCheckFill />
+            <div>
             <h4>Tailwind</h4>
             <small className='text-light'>Experienced</small>
+            </div>
             </article>
             <article className='experience__details'>
             <BsFillPatchCheckFill />
+            <div>
             <h4>React</h4>
             <small className='text-light'>Experienced</small>
+            </div>
             </article>
           </div>
 
@@ -57,33 +69,45 @@ const Experience = () => {
         <div className="experience__content">
             <article className='experience__details'>
             <BsFillPatchCheckFill />
-            <h4>HTML</h4>
+            <div>
+            <h4>Node JS</h4>
             <small className='text-light'>Experienced</small>
+            </div>
             </article>
             <article className='experience__details'>
             <BsFillPatchCheckFill />
-            <h4>CSS</h4>
-            <small className='text-light'>Intermediate</small>
+             <div>
+             <h4>Express JS</h4>
+             <small className='text-light'>Intermediate</small>
+             </div>
             </article>
             <article className='experience__details'>
             <BsFillPatchCheckFill />
-            <h4>JavaScript</h4>
+            <div>
+            <h4>MongoDB</h4>
             <small className='text-light'>Expert</small>
+            </div>
             </article>
             <article className='experience__details'>
             <BsFillPatchCheckFill />
-            <h4>Bootstrap</h4>
-            <small className='text-light'>Experienced</small>
+             <div>
+             <h4>MySQL </h4>
+             <small className='text-light'>Basic</small>
+             </div>
             </article>
             <article className='experience__details'>
             <BsFillPatchCheckFill />
-            <h4>Tailwind</h4>
-            <small className='text-light'>Experienced</small>
+             <div>
+             <h4>Git & Github</h4>
+             <small className='text-light'>Experienced</small>
+             </div>
             </article>
             <article className='experience__details'>
             <BsFillPatchCheckFill />
-            <h4>React</h4>
+            <div> 
+            <h4>Oracle</h4>
             <small className='text-light'>Experienced</small>
+            </div>
             </article>
           </div>
         </div>
