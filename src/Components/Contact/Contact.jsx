74da05ee30768d5b359e5897c69a7f5d2c8846ec
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-05 22:42:30 +0500

    comments some content

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import './Contact.css';
+// import React from 'react';
+// import './Contact.css';
 
 
-const Contact = () => {
-  return (
-    <section id='contact'>
-      Contact Section
-    </section>
-  )
-}
+// const Contact = () => {
+//   return (
+//     <section id='contact'>
+//       Contact Section
+//     </section>
+//   )
+// }
 
-export default Contact
+// export default Contact
+
+
