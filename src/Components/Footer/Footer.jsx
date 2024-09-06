edca093f09c02d399c148fc3832cace1681cf42f
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-06 18:17:49 +0500

    Remove Footer Section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,43 +1,43 @@
-// import React from 'react';
-// import './Footer.css';
-
-// const Footer = () => {
-//   return (
-//     <div>
-//       Footer Section
-//     </div>
-//   )
-// }
-
-// export default Footer
-
-
-
 import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer id='footer'>
-      <div className="container footer__container">
-        <h2 className="footer__title">Contact Me</h2>
-        <p className="footer__description">
-          I'm always open to new opportunities and collaborations. Feel free to reach out!
-        </p>
+    <div>
+      Footer Section
+    </div>
+  )
+}
 
-        <div className="footer__links">
-          <a href="mailto:sohaib.ahmed.work@gmail.com" className="footer__link">Email</a>
-          <a href="https://www.linkedin.com/in/syed-maaz-saeed" target="_blank" rel="noopener noreferrer" className="footer__link">LinkedIn</a>
-          <a href="https://github.com/SyedMaazSaeed" target="_blank" rel="noopener noreferrer" className="footer__link">GitHub</a>
-        </div>
+export default Footer
 
-        <p className="footer__copy">
-          &copy; {new Date().getFullYear()} Syed Maaz Saeed. All rights reserved.@Developer
-        </p>
-      </div>
-    </footer>
-  );
-};
 
-export default Footer;
+
+// import React from 'react';
+// import './Footer.css';
+
+// const Footer = () => {
+//   return (
+//     <footer id='footer'>
+//       <div className="container footer__container">
+//         <h2 className="footer__title">Contact Me</h2>
+//         <p className="footer__description">
+//           I'm always open to new opportunities and collaborations. Feel free to reach out!
+//         </p>
+
+//         <div className="footer__links">
+//           <a href="mailto:sohaib.ahmed.work@gmail.com" className="footer__link">Email</a>
+//           <a href="https://www.linkedin.com/in/syed-maaz-saeed" target="_blank" rel="noopener noreferrer" className="footer__link">LinkedIn</a>
+//           <a href="https://github.com/SyedMaazSaeed" target="_blank" rel="noopener noreferrer" className="footer__link">GitHub</a>
+//         </div>
+
+//         <p className="footer__copy">
+//           &copy; {new Date().getFullYear()} Syed Maaz Saeed. All rights reserved.@Developer
+//         </p>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
 
