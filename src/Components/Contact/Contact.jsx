785126a1d4705743f6612ea60b68175344abc716
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-06 18:20:34 +0500

    remove contact section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,53 +1,53 @@
-// import React from 'react';
-// import './Contact.css';
-
-
-// const Contact = () => {
-//   return (
-//     <section id='contact'>
-//       Contact Section
-//     </section>
-//   )
-// }
-
-// export default Contact
-
-
 import React from 'react';
 import './Contact.css';
+
 
 const Contact = () => {
   return (
     <section id='contact'>
-      <h5>Get In Touch</h5>
-      <h2>Contact Me</h2>
-
-      <div className="container contact__container">
-        <div className="contact__info">
-          <article className="contact__details">
-            <h4>Name</h4>
-            <p>Syed Maaz Saeed</p>
-          </article>
-          
-          <article className="contact__details">
-            <h4>Email</h4>
-            <a href="mailto:syedmaazsaeed@gmail.com">syedmaazsaeed@gmail.com</a>
-          </article>
-
-          <article className="contact__details">
-            <h4>Phone</h4>
-            <p>0310-6902002</p>
-          </article>
-
-          <article className="contact__details">
-            <h4>LinkedIn</h4>
-            <a href="https://www.linkedin.com/in/syedmaazsaeed/" target="_blank" rel="noopener noreferrer">syedmaazsaeed</a>
-          </article>
-        </div>
-      </div>
+      Contact Section
     </section>
   )
 }
 
-export default Contact;
+export default Contact
+
+
+// import React from 'react';
+// import './Contact.css';
+
+// const Contact = () => {
+//   return (
+//     <section id='contact'>
+//       <h5>Get In Touch</h5>
+//       <h2>Contact Me</h2>
+
+//       <div className="container contact__container">
+//         <div className="contact__info">
+//           <article className="contact__details">
+//             <h4>Name</h4>
+//             <p>Syed Maaz Saeed</p>
+//           </article>
+          
+//           <article className="contact__details">
+//             <h4>Email</h4>
+//             <a href="mailto:syedmaazsaeed@gmail.com">syedmaazsaeed@gmail.com</a>
+//           </article>
+
+//           <article className="contact__details">
+//             <h4>Phone</h4>
+//             <p>0310-6902002</p>
+//           </article>
+
+//           <article className="contact__details">
+//             <h4>LinkedIn</h4>
+//             <a href="https://www.linkedin.com/in/syedmaazsaeed/" target="_blank" rel="noopener noreferrer">syedmaazsaeed</a>
+//           </article>
+//         </div>
+//       </div>
+//     </section>
+//   )
+// }
+
+// export default Contact;
 
