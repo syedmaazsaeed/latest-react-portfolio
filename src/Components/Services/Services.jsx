b3b6a7f6ecb3e3ccd4215fe515ebae42c27a581f
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-07 15:38:35 +0500

    add code into service section

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,10 +1,148 @@
 import React from 'react';
 import './Services.css';
+import { IoShieldCheckmarkSharp } from "react-icons/io5";
+
 
 const Services = () => {
   return (
     <section id='services'>
-      Services Section
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+     
+     <div className="container services__container">
+
+      <article className="service">
+
+        <div className="service__head">
+          <h3>UI/UX Design</h3>
+        </div>
+
+        <ul className='service__list'>
+          <li>
+          <IoShieldCheckmarkSharp className='service__list-icon' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur elit.
+          </p>
+          </li>
+          <li>
+          <IoShieldCheckmarkSharp className='service__list-icon' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur elit.
+          </p>
+          </li>
+          <li>
+          <IoShieldCheckmarkSharp className='service__list-icon' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur elit.
+          </p>
+          </li>
+          <li>
+          <IoShieldCheckmarkSharp className='service__list-icon' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur elit.
+          </p>
+          </li>
+          <li>
+          <IoShieldCheckmarkSharp className='service__list-icon' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur elit.
+          </p>
+          </li>
+          
+        </ul>
+      </article>
+
+      {/* Website Development */}
+
+      <article className="service">
+
+<div className="service__head">
+  <h3>Web Development</h3>
+</div>
+
+<ul className='service__list'>
+  <li>
+  <IoShieldCheckmarkSharp className='service__list-icon' />
+  <p>
+    Lorem ipsum dolor sit amet consectetur elit.
+  </p>
+  </li>
+  <li>
+  <IoShieldCheckmarkSharp className='service__list-icon' />
+  <p>
+    Lorem ipsum dolor sit amet consectetur elit.
+  </p>
+  </li>
+  <li>
+  <IoShieldCheckmarkSharp className='service__list-icon' />
+  <p>
+    Lorem ipsum dolor sit amet consectetur elit.
+  </p>
+  </li>
+  <li>
+  <IoShieldCheckmarkSharp className='service__list-icon' />
+  <p>
+    Lorem ipsum dolor sit amet consectetur elit.
+  </p>
+  </li>
+  <li>
+  <IoShieldCheckmarkSharp className='service__list-icon' />
+  <p>
+    Lorem ipsum dolor sit amet consectetur elit.
+  </p>
+  </li>
+  <li>
+  <IoShieldCheckmarkSharp className='service__list-icon' />
+  <p>
+    Lorem ipsum dolor sit amet consectetur elit.
+  </p>
+  </li>
+  
+</ul>
+</article>
+
+<article className="service">
+
+<div className="service__head">
+  <h3>Software Developer</h3>
+</div>
+
+<ul className='service__list'>
+  <li>
+  <IoShieldCheckmarkSharp className='service__list-icon' />
+  <p>
+    Lorem ipsum dolor sit amet consectetur elit.
+  </p>
+  </li>
+  <li>
+  <IoShieldCheckmarkSharp className='service__list-icon' />
+  <p>
+    Lorem ipsum dolor sit amet consectetur elit.
+  </p>
+  </li>
+  <li>
+  <IoShieldCheckmarkSharp className='service__list-icon' />
+  <p>
+    Lorem ipsum dolor sit amet consectetur elit.
+  </p>
+  </li>
+  <li>
+  <IoShieldCheckmarkSharp className='service__list-icon' />
+  <p>
+    Lorem ipsum dolor sit amet consectetur elit.
+  </p>
+  </li>
+  <li>
+  <IoShieldCheckmarkSharp className='service__list-icon' />
+  <p>
+    Lorem ipsum dolor sit amet consectetur elit.
+  </p>
+  </li>
+</ul>
+</article>
+{/* END Of Software Development */}
+
+     </div>
     </section>
   )
 }
