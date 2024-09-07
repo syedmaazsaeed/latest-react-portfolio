b83a6df4056493caf04711a27e09355e40eb6026
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-07 16:08:43 +0500

    add service secction

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -14,7 +14,7 @@ const Services = () => {
       <article className="service">
 
         <div className="service__head">
-          <h3>UI/UX Design</h3>
+          <h3>Programmer</h3>
         </div>
 
         <ul className='service__list'>
@@ -104,7 +104,7 @@ const Services = () => {
 <article className="service">
 
 <div className="service__head">
-  <h3>Software Developer</h3>
+  <h3>Software Development</h3>
 </div>
 
 <ul className='service__list'>
