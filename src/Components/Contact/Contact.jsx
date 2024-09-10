11cb4a3aadf30c9c4590969f8716a3f965213b7c
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-11 00:40:58 +0500

    starting contact section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,7 +6,20 @@ import './Contact.css';
 const Contact = () => {
   return (
     <section id='contact'>
-      Contact Section
+      <h5>Get In Touch With me</h5>
+      <h2>Contact me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+         
+         <article className="contact__option">
+          <h4>Email</h4>
+          
+         </article>
+        </div>
+        {/* Contact Section Ended */}
+        <form action=""></form>
+      </div>
+
     </section>
   )
 }
