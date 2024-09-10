402bd21ea5392ea8a14df246e099259cb05e0d8b
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-10 22:20:24 +0500

    remove hard coded content

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -48,40 +48,7 @@ const Testimonials = () => {
                       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.
                     </small>
             </article>
-            <article className="testimonial">
-                <div className="client__avatar">
-                    <img src={avatar1} alt="Avatar 1" />             
-                </div>
-                <h5 className='client__name'>
-                      Syed Maaz Saeed
-                    </h5>
-                    <small className='client__review'>
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.
-                    </small>
-            </article>
-            <article className="testimonial">
-                <div className="client__avatar">
-                    <img src={avatar1} alt="Avatar 1" />             
-                </div>
-                <h5 className='client__name'>
-                      Syed Maaz Saeed
-                    </h5>
-                    <small className='client__review'>
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.
-                    </small>
-            </article>
-            <article className="testimonial">
-                <div className="client__avatar">
-                    <img src={avatar1} alt="Avatar 1" />             
-                </div>
-                <h5 className='client__name'>
-                      Syed Maaz Saeed
-                    </h5>
-                    <small className='client__review'>
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.
-                    </small>
-            </article>
-
+ 
          </div>
 
 
