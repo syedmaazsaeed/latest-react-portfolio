bdeefe3d1cddbd7bf95ed17b9107f5e415ca75c1
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-10 18:48:51 +0500

    add style section in my testimonials file

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import avatar from '../../assets/';
+import './Testimonials.css' ;
+import avatar1 from '../../assets/me.jpg';
+import avatar2 from '../../assets/three.jpeg';
+import avatar3 from '../../assets/three.jpeg';
+import avatar4 from '../../assets/three.jpeg';
+
 
 const Testimonials = () => {
   return (
@@ -10,9 +15,38 @@ const Testimonials = () => {
          <div className="container testimonials__container">
             <article className="testimonial">
                 <div className="client__avatar">
-                    <img src={avatar} alt="Avatar 1" />
+                    <img src={avatar1} alt="Avatar 1" />             
                 </div>
+                <h5 className='client__name'>
+                      Ernest Achiever
+                    </h5>
+                    <small className='client__review'>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.
+                    </small>
             </article>
+            <article className="testimonial">
+                <div className="client__avatar">
+                    <img src={avatar1} alt="Avatar 1" />             
+                </div>
+                <h5 className='client__name'>
+                      Ernest Achiever
+                    </h5>
+                    <small className='client__review'>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.
+                    </small>
+            </article>
+            <article className="testimonial">
+                <div className="client__avatar">
+                    <img src={avatar1} alt="Avatar 1" />             
+                </div>
+                <h5 className='client__name'>
+                      Ernest Achiever
+                    </h5>
+                    <small className='client__review'>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.
+                    </small>
+            </article>
+
          </div>
 
 
