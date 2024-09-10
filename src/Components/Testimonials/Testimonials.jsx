c011ef162de120da00788268da738f71b7d8a74d
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-10 22:19:09 +0500

    add  content

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -5,7 +5,31 @@ import avatar2 from '../../assets/three.jpeg';
 import avatar3 from '../../assets/three.jpeg';
 import avatar4 from '../../assets/three.jpeg';
 
+const data = [
 
+  {avatar: avatar1,
+    name: 'James Sunaty',
+    review: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.'
+  },
+  {avatar: avatar1,
+    name: 'Tonny James',
+    review: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.'
+  },
+  {avatar: avatar1,
+    name: 'Ich bin silwa',
+    review: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.'
+  },
+  {avatar: avatar1,
+    name: 'Hashir hussain',
+    review: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.'
+  },
+  {avatar: avatar1,
+    name: 'Ali Singh',
+    review: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.'
+  },
+
+
+]
 const Testimonials = () => {
   return (
     <section id='testimonials'>
@@ -18,7 +42,7 @@ const Testimonials = () => {
                     <img src={avatar1} alt="Avatar 1" />             
                 </div>
                 <h5 className='client__name'>
-                      Ernest Achiever
+                      Syed Maaz Saeed
                     </h5>
                     <small className='client__review'>
                       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.
@@ -29,7 +53,7 @@ const Testimonials = () => {
                     <img src={avatar1} alt="Avatar 1" />             
                 </div>
                 <h5 className='client__name'>
-                      Ernest Achiever
+                      Syed Maaz Saeed
                     </h5>
                     <small className='client__review'>
                       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.
@@ -40,7 +64,18 @@ const Testimonials = () => {
                     <img src={avatar1} alt="Avatar 1" />             
                 </div>
                 <h5 className='client__name'>
-                      Ernest Achiever
+                      Syed Maaz Saeed
+                    </h5>
+                    <small className='client__review'>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.
+                    </small>
+            </article>
+            <article className="testimonial">
+                <div className="client__avatar">
+                    <img src={avatar1} alt="Avatar 1" />             
+                </div>
+                <h5 className='client__name'>
+                      Syed Maaz Saeed
                     </h5>
                     <small className='client__review'>
                       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque porro quae vel voluptatem facilis? Temporibus a est quos cumque, tempore, recusandae ipsa ullam consequatur quaerat corrupti provident voluptatem sint sunt.
