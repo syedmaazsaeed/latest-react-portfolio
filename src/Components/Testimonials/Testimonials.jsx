cd0a4e477142b6e27af4623a9482ca029a585ce1
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-10 13:59:19 +0500

    setting testimonials section with styling

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import avatar from '../../assets/';
 
 const Testimonials = () => {
   return (
-    <div>
-      
-    </div>
+    <section id='testimonials'>
+        <h5>Review From Clients</h5>
+        <h2>Testimonials</h2>
+
+         <div className="container testimonials__container">
+            <article className="testimonial">
+                <div className="client__avatar">
+                    <img src={avatar} alt="Avatar 1" />
+                </div>
+            </article>
+         </div>
+
+
+    </section>
   )
 }
 
