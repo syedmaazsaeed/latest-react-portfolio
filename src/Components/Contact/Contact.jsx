d7cf9bfeba0f472cfae11f9da49bbeae57df8bcd
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-11 20:41:18 +0500

    add contact sections

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import './Contact.css';
+import { MdOutlineMarkEmailUnread } from "react-icons/md";
+import { FaInstagram } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { BsWhatsapp } from "react-icons/bs";
+import { GrLinkedin } from "react-icons/gr";
+import { SiBuymeacoffee } from "react-icons/si";
 
 
 
@@ -12,13 +18,51 @@ const Contact = () => {
         <div className="contact__options">
          
          <article className="contact__option">
+         <MdOutlineMarkEmailUnread />
           <h4>Email</h4>
+          <h5>syedmaazsaeed@gmail.com</h5>
+          <a href="mailto:syedmaazsaeed@gmail.com" target="_blank" >Drop a message</a>
+          
+         </article>
+         <article className="contact__option">
+         <GrLinkedin />
+          <h4>Linkedin</h4>
+          <h5>syedmaazsaeed</h5>
+          <a href="https://www.linkedin.com/in/syed-maaz-saeed-908718204/" target="_blank" >Drop a message</a>
+          
+         </article>
+         <article className="contact__option">
+         <FaXTwitter />
+          <h4>Twitter</h4>
+          <h5>SyedMaazSaeed</h5>
+          <a href="https://twitter.com/SyedMaazSaeed" target="_blank">Drop a message</a>
+          
+         </article>
+         <article className="contact__option">
+         <FaInstagram />
+          <h4>Instagram</h4>
+          <h5>syedmaazsaeed</h5>
+          <a href="https://www.instagram.com/syedmaazsaeed/" target="_blank" >Drop a message</a>
+          
+         </article>
+         <article className="contact__option">
+         <SiBuymeacoffee />
+          <h4>buymeacoffee</h4>
+          <h5>syedmaazsaeed0</h5>
+          <a href="https://buymeacoffee.com/syedmaazsaeed0" target="_blank" >Drop a message</a>
+          
+         </article>
+         <article className="contact__option">
+         <BsWhatsapp />
+          <h4>Whatsapp</h4>
+          <h5>+92-310-690200-2</h5>
+          <a href="https://api.whatsapp.com/send?phone=923106902002" target="_blank" >Drop a message</a>
           
          </article>
         </div>
         {/* Contact Section Ended */}
         <form action="">
-          <h2>Password</h2>
+          
         </form>
       </div>
 
