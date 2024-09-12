e9aa0b8d63de8121d833d6e31d3e7ea301d4ed5f
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-12 20:40:26 +0500

    set footer section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './Footer.css'; // Make sure your CSS is properly linked
-import { motion } from 'framer-motion'; // Framer Motion for animation
+import './Footer.css';
+import { motion } from 'framer-motion';
 import { FaFacebook, FaGithub } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa6';
 import { IoLogoLinkedin } from 'react-icons/io5';
 import { FaSquareXTwitter } from 'react-icons/fa6';
 import { SiFiverr, SiBuymeacoffee } from 'react-icons/si';
+
 
 const Footer = () => {
   return (
@@ -107,53 +108,4 @@ const Footer = () => {
 export default Footer;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import './Footer.css';
-// import Experience from './../Experience/Experience';
-// import About from './../About/About';
-
-// const Footer = () => {
-//   return (
-//     <footer id='footer'>
-//       <div className="container footer__container">
-//         <h2 className="footer__title">Contact Me</h2>
-//         <p className="footer__description">
-//           I'm always open to new opportunities and collaborations. Feel free to reach out!
-//         </p>
-
-//         <div className="footer__links">
-//           <a href="mailto:sohaib.ahmed.work@gmail.com" className="footer__link">Email</a>
-//           <a href="https://www.linkedin.com/in/syed-maaz-saeed" target="_blank" rel="noopener noreferrer" className="footer__link">LinkedIn</a>
-//           <a href="https://github.com/SyedMaazSaeed" target="_blank" rel="noopener noreferrer" className="footer__link">GitHub</a>
-//         </div>
-
-//         <p className="footer__copy">
-//           &copy; {new Date().getFullYear()} Syed Maaz Saeed. All rights reserved.@Developer
-//         </p>
-//       </div>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
 
