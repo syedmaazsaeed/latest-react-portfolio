76d163de3d5d609c1e8513217693fe3252de666d
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-12 20:37:58 +0500

    add water animation effect

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,54 +1,112 @@
 import React from 'react';
-import './Footer.css';
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa6";
-import { IoLogoLinkedin } from "react-icons/io5";
-import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaGithub } from "react-icons/fa";
-import { FaSquareUpwork } from "react-icons/fa6";
-import { SiFiverr } from "react-icons/si";
-import { SiBuymeacoffee } from "react-icons/si";
-
-
-
- 
+import './Footer.css'; // Make sure your CSS is properly linked
+import { motion } from 'framer-motion'; // Framer Motion for animation
+import { FaFacebook, FaGithub } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa6';
+import { IoLogoLinkedin } from 'react-icons/io5';
+import { FaSquareXTwitter } from 'react-icons/fa6';
+import { SiFiverr, SiBuymeacoffee } from 'react-icons/si';
 
 const Footer = () => {
   return (
-    <footer>
-      <a href="#" className='footer__logo'>SyedMaazSaeed</a>
+    <motion.footer
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.4, delay: 0.3 }}
+      id="footer"
+      className="footer"
+    >
+      <div className="waves">
+        <div className="wave" id="wave1"></div>
+        <div className="wave" id="wave2"></div>
+        <div className="wave" id="wave3"></div>
+        <div className="wave" id="wave4"></div>
+      </div>
+      
+      <div className="footer__text__container">
+        <ul className="social-icon">
+          <li className="social-icon__item">
+            <a className="social-icon__link" href="https://facebook.com">
+              <FaFacebook />
+            </a>
+          </li>
+          <li className="social-icon__item">
+            <a className="social-icon__link" href="https://instagram.com">
+              <FaInstagram />
+            </a>
+          </li>
+          <li className="social-icon__item">
+            <a className="social-icon__link" href="https://twitter.com">
+              <FaSquareXTwitter />
+            </a>
+          </li>
+          <li className="social-icon__item">
+            <a className="social-icon__link" href="https://github.com">
+              <FaGithub />
+            </a>
+          </li>
+          <li className="social-icon__item">
+            <a className="social-icon__link" href="https://linkedin.com">
+              <IoLogoLinkedin />
+            </a>
+          </li>
+          <li className="social-icon__item">
+            <a className="social-icon__link" href="https://fiverr.com">
+              <SiFiverr />
+            </a>
+          </li>
+          <li className="social-icon__item">
+            <a className="social-icon__link" href="https://buymeacoffee.com/syedmaazsaeed0">
+              <SiBuymeacoffee />
+            </a>
+          </li>
+        </ul>
 
-      <ul className='permalinks'>
-        <li><a href="#">Home </a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#Portfolio">Portfolio </a></li>
-        <li><a href="#testimonials ">Testimonials </a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-
-
-      <div className="footer__socials">
-        <a href="https://facebook.com"> <FaFacebook /> </a>
-        <a href="https://instagram.com"> <FaInstagram/> </a>
-        <a href="https://twitter.com"> <FaSquareXTwitter /> </a>
-        <a href="https://github.com"> <FaGithub /> </a>
-        <a href="https://linkedin.com"> <IoLogoLinkedin /> </a>
-        <a href="https://upwork.com"> <FaSquareUpwork /> </a>
-        <a href="https://fiver.com"> <SiFiverr /> </a>
-        <a href="https://buymeacoffee.com/syedmaazsaeed0" target="_blank" > <SiBuymeacoffee/> </a>
+        <ul className="menu">
+          <li className="menu__item">
+            <a className="menu__link" href="#">
+              Home
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="#about">
+              About
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="#experience">
+              Experience
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="#services">
+              Services
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="#Portfolio">
+              Portfolio
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
       </div>
 
-      <div className="footer__copyright">
-        <small>
-          &copy; Syed Maaz Saeed All rights reserved.
-        </small>
-      </div>
-    </footer>
-  )
-}
-export default Footer
+      <p className="copyright">
+        &copy;2024 Syed Maaz Saeed | All Rights Reserved
+      </p>
+    </motion.footer>
+  );
+};
+
+export default Footer;
+
+
 
 
 
