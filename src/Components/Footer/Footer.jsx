7f685cdf244804b3c29c386e194ea734995d4945
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-13 23:30:50 +0500

    set footer section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -109,3 +109,39 @@ export default Footer;
 
 
 
+
+
+
+
+import React from 'react';
+import Header from './Components/Header/Header';
+import Nav from './Components/Nav/Nav';
+import About from './Components/About/About';
+import Experience from './Components/Experience/Experience';
+import Services from './Components/Services/Services';
+import Portfolio from './Components/Portfolio/Portfolio';
+import Testimonials from './Components/Testimonials/Testimonials';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+import ScrollProgressBar from './Components/ScrollProgressbar/ScrollProgressBar';
+import SkillsChart from './Components/SkillsChart/SkillsChart'; // Import the SkillsChart component
+
+const App = () => {
+  return (
+    <>
+      <ScrollProgressBar />
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <SkillsChart /> {/* Add the SkillsChart component here */}
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
