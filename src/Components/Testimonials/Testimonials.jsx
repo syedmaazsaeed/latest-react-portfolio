61061341f76b243e802ba0e9bebe22487bcc3884
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-13 18:20:53 +0500

    set testimonials section

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -5,19 +5,14 @@ import avatar2 from '../../assets/three.jpeg';
 import avatar3 from '../../assets/three.jpeg';
 import avatar4 from '../../assets/three.jpeg';
 
-
-// import Swiper core and required modules
 import {Pagination, Autoplay } from 'swiper/modules';
-
-// import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-// import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-// import 'swiper/css/scrollbar';
+
 const data = [
 
   {avatar: avatar1,
@@ -53,17 +48,12 @@ const Testimonials = () => {
 
          // install Swiper modules
       modules={[ Pagination, Autoplay ]}
-      // modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={40}
       slidesPerView={1}
       // navigation
       autoplay={{ delay: 4000 }} 
 
       pagination={{ clickable: true }}
-      // scrollbar={{ draggable: true }}
-      // onSwiper={(swiper) => console.log(swiper)}
-      // onSlideChange={() => console.log('slide change')}
-         
          >
             
             {
