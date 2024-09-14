16cb470fa1f791588470ae9889abe6eb2eef2cdd
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-14 19:37:20 +0500

    add

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,7 +9,6 @@ import { FaAward } from "react-icons/fa6";
 import { HiUsers } from "react-icons/hi2";
 import { FaCircleCheck } from "react-icons/fa6";
 
-
 const About = () => {
   // { // Array of images }
   const images = [ME, ME2, ME3, ME4];
@@ -70,7 +69,7 @@ const About = () => {
           I am currently Working as a MERN Stack Technology.As a self-taught Front-End Developer | React Developer I specialize in modern web technologies, including React Node Express JS and MongoDB.I have completed (ICS) in Maths and Computer at Superior Group of College Bahawalpur.
           I am currently delving deep into the field of software engineering at the Islamia University of Bahawalpur, where I am eager to explore the new technological era and stay consistently updated to remain at the forefront of innovation and development.
        </p>
-
+         
        <a href="#contact" className='btn btn-primary btn-swing'>Let's Connect</a>
         </div>
       </div>
