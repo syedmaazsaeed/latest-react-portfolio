86f6779d8c9e5d0b51ed6f229b8de4cac7c8e652
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-09-15 13:26:19 +0500

    add links

--- src/Components/Header/HeaderSocials.jsx
+++ src/Components/Header/HeaderSocials.jsx
@@ -11,11 +11,11 @@ import { FaSquareUpwork } from "react-icons/fa6";
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href="https://linkedin.com" target="_blank"><GrLinkedin /> </a>
-      <a href="https://gitub.com" target="_blank"><FaGithubSquare />  </a>
-      <a href="https://instagram.com" target="_blank"><TbBrandFiverr /> </a>
-      <a href="https://twitter.com" target="_blank"><FaSquareXTwitter /></a>
-      <a href="https://twitter.com" target="_blank"><FaSquareUpwork /></a>
+      <a href="https://www.linkedin.com/in/syed-maaz-saeed-908718204/" target="_blank"  rel="noreferrer"><GrLinkedin /> </a>
+      <a href="https://github.com/syedmaazsaeed" target="_blank"  rel="noreferrer"><FaGithubSquare />  </a>
+      <a href="https://www.fiverr.com/maaz878/buying?source=avatar_menu_profile" target="_blank"  rel="noreferrer"><TbBrandFiverr /> </a>
+      <a href="https://x.com/SyedMaazSaeed" target="_blank"  rel="noreferrer"><FaSquareXTwitter /></a>
+      <a href="https://www.upwork.com/freelancers/~01107293c0befdc9ae" target="_blank"  rel="noreferrer"><FaSquareUpwork /></a>
     </div>
   )
 }
